2518b92e8bf585c9e0a8edcc880582655ef01657
Jack Warrick <jackwarrick@Jacks-MBP.zyxel.com>
2022-11-29 23:35:52 -0600

    minor tweaks before done for the day

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -6,7 +6,7 @@ export const Project = ({ url, title, github, description }) => {
       <h5 className="card-title">{title}</h5>
       <h6 className="card-subtitle mb-2 text-muted">{description}</h6>
       <a href={url} className="card-link">Deployed App</a>
-      <a href={github} className="card-link">GitHub repository for the App</a>
+      <a href={github} className="card-link">GitHub repository</a>
     </div>
   </div>
 
