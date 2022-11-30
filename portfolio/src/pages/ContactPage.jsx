ad46e3143637600db3ff5ed272678f70182419c0
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2022-11-30 17:50:19 -0600

    changed the files to make them OK to turn in

--- portfolio/src/pages/ContactPage.jsx
+++ portfolio/src/pages/ContactPage.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export const ContactPage = () => {
     return <section>
         <h1>Here is my contact information</h1>
+        <p>Email: j.d.warrick@comcast.net</p>
+        <p>Cell: 651-261-2464</p>
     </section>
 
 }
--- portfolio/src/pages/ResumePage.jsx
+++ portfolio/src/pages/ResumePage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export const ResumePage = () => {
     return <section>
         <h1>Here is my Resume</h1>
+        <p>I've done some pretty cool stuff.</p>
     </section>
 
 }
