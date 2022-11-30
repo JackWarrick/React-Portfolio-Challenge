92152e8432a0f023ee23971c921879394b67f395
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2022-11-29 20:02:15 -0600

    needed to start over again and fix react dom client error

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,57 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import { Header } from './components/Header';
-import { Nav } from './components/Nav';
-import { Footer } from './components/Footer';
 
-
-//Will have the pages of about, portfolio, contact, and resume  --  we will begin with the about page
-function App(){
-
-    const [page, setPage] = useState("about");
-
-    const currentPage = () => {
-        // switch(page){
-        //     case "portfolio":
-        //         return <PortfolioPage />
-        //     case "contact":
-        //         return <ContactPage />
-        //     case "resume":
-        //         return <ResumePage />
-        //     case "about":
-        //         return <AboutPage />
-        //     default:
-        //         return <AboutPage />
-        // }
-
-    }
-
-    //Header, Nav, and Footer caps because they are brought in
-    return (
-        <div className="App">
-
-            <Header className="App-header">
-
-            <img src={logo} className="App-logo" alt="logo" />
-
-            <p> Not Sure What We Need To Put Here - p and img can possibly be deleted</p>
-
-            <Nav page={page} setPage={setPage} />
-
-            </Header>
-
-            <main>
-                {currentPage()}
-            </main>
-
-            <Footer>
-
-            </Footer>
-            
-        </div>
-
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
